d79c59643a49dbe45cbe610ca4cabca6947dd30a
erictherobot <erictherobot@gmail.com>
2019-03-23 16:14:53 -0400

    Adding some style tweaks

--- src/config/ThemeStyles.ts
+++ src/config/ThemeStyles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet, TextStyle, ViewStyle } from 'react-native'
 import RF from 'react-native-responsive-fontsize'
-import { verticalScale } from 'react-native-size-matters'
+import { verticalScale, moderateScale } from 'react-native-size-matters'
 
 import { THEME_BG_COLOR, THEME_FG_COLOR } from './Colors'
 
@@ -13,21 +13,24 @@ export const ThemeStyles = StyleSheet.create({
   } as ViewStyle,
   title: {
     fontSize: RF(2.8),
-    fontWeight: 'bold',
+    fontWeight: '600',
     textAlign: 'center',
     color: THEME_FG_COLOR,
     marginBottom: verticalScale(28),
+    marginHorizontal: moderateScale(20),
   } as TextStyle,
   tagline: {
     fontSize: RF(2.5),
     textAlign: 'center',
     color: THEME_FG_COLOR,
     marginBottom: verticalScale(12),
+    marginHorizontal: moderateScale(20),
   } as TextStyle,
   instructions: {
     textAlign: 'center',
     color: THEME_FG_COLOR,
     marginBottom: verticalScale(12),
+    marginHorizontal: moderateScale(20),
   } as TextStyle,
 })
 
